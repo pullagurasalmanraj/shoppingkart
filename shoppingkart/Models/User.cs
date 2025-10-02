ff8ae75beb4353e825a313520887ada66b30f899
pullagurasalmanraj <160998280+pullagurasalmanraj@users.noreply.github.com>
2025-10-02 10:09:24 +0530

    Delete shoppingkart directory

--- shoppingkart/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class User
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
